e5d0d8d3f075de5f9f98c643d7460d2e5edd24ee
Andre Chaves <andrechavesg@gmail.com>
2017-10-24 12:12:56 -0200

    capitulo 4

--- forca.c
+++ forca.c
@@ -1,44 +1,71 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
 
-    char palavrasecreta[20];
+void abertura() {
+    printf("/****************/\n");
+    printf("/ Jogo de Forca */\n");
+    printf("/****************/\n\n");
+}
+
+void chuta() {
+    char chute;
+    printf("Qual letra? ");
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+}
+
+int jachutou(char letra) {
+    int achou = 0;
+    for(int j = 0; j < tentativas; j++) {
+        if(chutes[j] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+void desenhaforca() {
+
+    printf("Você já deu %d chutes\n", tentativas);
+
+    for(int i = 0; i < strlen(palavrasecreta); i++) {
+
+        if(jachutou(palavrasecreta[i])) {
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+
+    }
+    printf("\n");
+
+}
+
+void escolhepalavra() {
     sprintf(palavrasecreta, "MELANCIA");
+}
+
+int main() {
 
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
+    abertura();
+    escolhepalavra();
 
     do {
 
-        for(int i = 0; i < strlen(palavrasecreta); i++) {
-            int achou = 0;
+        desenhaforca();
+        chuta();
 
-            for(int j = 0; j < tentativas; j++) {
-                if(chutes[j] == palavrasecreta[i]) {
-                    achou = 1;
-                    break;
-                }
-            }
-
-            if(achou) {
-                printf("%c ", palavrasecreta[i]);
-            } else {
-                printf("_ ");
-            }
-        }
-        printf("\n");
-
-        char chute;
-        printf("Qual letra? ");
-        scanf(" %c", &chute);
-
-        chutes[tentativas] = chute;
         tentativas++;
-
 
     } while (!acertou && !enforcou);
 
